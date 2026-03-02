f1c0786d02641cf5b742787420cb03b9d4521077
Etienne Camenen <etienne.camenen@gmail.com>
2026-03-02 11:56:06 +0100

    Add percent_hemoglobin to qc

--- R/plot.R
+++ R/plot.R
@@ -2,7 +2,7 @@
 plot_sc_violin <- function(
         x,
         assay = "RNA",
-        features = c(paste0("nFeature", "_", assay), paste0("nCount", "_", assay), "percent_mitochondrial", "percent_ribosomal"),
+        features = c(paste0("nFeature", "_", assay), paste0("nCount", "_", assay), "percent_mitochondrial", "percent_ribosomal", "percent_hemoglobin"),
         normalize = NULL,
         nrow = 3,
         ncol = 3,
