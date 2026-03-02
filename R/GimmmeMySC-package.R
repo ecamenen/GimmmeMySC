b858353bba198783670812291fa9e2119cc55f02
Etienne Camenen <etienne.camenen@gmail.com>
2026-03-02 17:26:31 +0100

    Fix DoubletFinder with split
    * Add scales for variable_plot

--- R/GimmmeMySC-package.R
+++ R/GimmmeMySC-package.R
@@ -1,7 +1,7 @@
 #' @rawNamespace import(cowplot, except = c(save_plot))
 #' @rawNamespace import(patchwork, except = c(align_plots))
 #' @import clustree GimmeMyOmics GimmeMyPlot ggraph gridExtra kableExtra
-#' @import magrittr pheatmap RColorBrewer rlist
+#' @import magrittr pheatmap RColorBrewer rlist scales
 #' @import scDblFinder Seurat Signac SingleR tidyverse
 #' @keywords internal
 "_PACKAGE"
